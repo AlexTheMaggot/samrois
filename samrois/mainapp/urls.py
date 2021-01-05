ae52fce8740f23974121835207224c88f18965cd
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-01-06 04:04:20 +0500

    Add index urls

---
+++ samrois/mainapp/urls.py
@@ -0,0 +1,44 @@
+# DjangoImports
+
+from django.urls import path
+
+# End DjangoImports
+
+
+# InternalImports
+
+from . import views
+
+# End InteralImports
+
+
+# UrlPatterns
+
+urlpatterns = [
+    path('', views.redirect_to_index),
+
+
+    # RU
+
+    path('ru/', views.index, name='ru_index'),
+
+    # End RU
+
+
+    # EN
+
+    path('en/', views.index, name='en_index'),
+
+    # End EN
+
+
+    # KR
+
+    path('kr/', views.index, name='kr_index'),
+
+    # End KR
+
+
+]
+
+# End UrlPatterns
