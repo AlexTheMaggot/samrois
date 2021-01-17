bb2d4f26c262e77675c1f638df2b45d83fae1270
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-01-18 00:13:06 +0500

    Register Slides and Tours

--- samrois/mainapp/admin.py
+++ samrois/mainapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Slide, Tour
 
-# Register your models here.
+
+class SlideConfig(admin.ModelAdmin):
+    list_display = ('pk', 'image')
+
+
+admin.site.register(Slide, SlideConfig)
+
+
+class TourConfig(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tour, TourConfig)
