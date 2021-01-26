cd6f5bff572156ee58cce7687d5bf8d7d6054b30
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-01-26 17:29:29 +0500

    add TourList and TourDetail urls

--- samrois/mainapp/urls.py
+++ samrois/mainapp/urls.py
@@ -12,15 +12,22 @@ urlpatterns = [
 
     # RU
     path('ru/', views.index, name='ru_index'),
+    path('ru/tours/', views.tour_list, name='ru_tour_list'),
+    path('ru/tours/<int:tour_id>/', views.tour_detail, name='ru_tour_detail'),
     # End RU
 
     # EN
     path('en/', views.index, name='en_index'),
+    path('en/tours/', views.tour_list, name='en_tour_list'),
+    path('en/tours/<int:tour_id>/', views.tour_detail, name='en_tour_detail'),
     # End EN
 
     # KO
     path('ko/', views.index, name='ko_index'),
+    path('ko/tours/', views.tour_list, name='ko_tour_list'),
+    path('ko/tours/<int:tour_id>/', views.tour_detail, name='ko_tour_detail'),
     # End KO
 
 ]
 # End UrlPatterns
+
