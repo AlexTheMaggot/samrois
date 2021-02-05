1d2f77ec86911232dc2b858a2094d7e454d3528e
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-02-05 11:23:32 +0500

    change order creating path

--- samrois/mainapp/urls.py
+++ samrois/mainapp/urls.py
@@ -9,13 +9,13 @@ from . import views
 # UrlPatterns
 urlpatterns = [
     path('', views.redirect_to_index),
-    path('order_add', views.order_add, name='order_create'),
 
     # RU
     path('ru/', views.index, name='ru_index'),
     path('ru/tours/', views.tour_list, name='ru_tour_list'),
     path('ru/tours/<int:tour_id>/', views.tour_detail, name='ru_tour_detail'),
     path('ru/thank-you/', views.thank_you, name='ru_thank_you'),
+    path('ru/order_add', views.order_add, name='ru_order_create'),
     # End RU
 
     # EN
@@ -23,6 +23,7 @@ urlpatterns = [
     path('en/tours/', views.tour_list, name='en_tour_list'),
     path('en/tours/<int:tour_id>/', views.tour_detail, name='en_tour_detail'),
     path('en/thank-you/', views.thank_you, name='en_thank_you'),
+    path('en/order_add', views.order_add, name='en_order_create'),
     # End EN
 
     # KO
@@ -30,6 +31,7 @@ urlpatterns = [
     path('ko/tours/', views.tour_list, name='ko_tour_list'),
     path('ko/tours/<int:tour_id>/', views.tour_detail, name='ko_tour_detail'),
     path('ko/thank-you/', views.thank_you, name='ko_thank_you'),
+    path('ko/order_add', views.order_add, name='ko_order_create'),
     # End KO
 
 ]
